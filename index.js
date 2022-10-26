5b0d259d49baa7ac9dec534068e68bd10896af81
rodcoding <rodcoding@gmail.com>
2022-10-26 14:28:54 +0200

    mise en place de la route ajouter une maison

---
+++ index.js
@@ -0,0 +1,43 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+server.use(middlewares)
+
+maison = new Map();
+
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+  console.log('test');
+})
+server.post('/modification',(req,res) =>{
+  console.log('test');
+})
+
+server.post('/add',(req,res) =>{
+  obj = {
+    adresse : req.body.adresse,
+    prix : req.body.prix,
+    locataire : req.body.locataire
+  }
+  maison.set(req.body.locataire,obj);
+})
+
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
